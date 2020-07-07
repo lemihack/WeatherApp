4164892d7b47f77082703b3bf67fef49876cee63
krisha <krisha@localhost.localdomain>
2020-07-07 07:58:54 -0400

    first commit

---
+++ forcast.js
@@ -0,0 +1,36 @@
+const key = 'N6JKlGxvWAjgSHEcF6aM1wJsdxFDP94m';
+
+
+
+const getWeather = async id => {
+
+
+    const baseUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const queryParam = `${id}?apikey=${key}`;
+
+    const response = await fetch(baseUrl + queryParam);
+    const data = await response.json();
+    return data[0];
+
+}
+
+
+const getCity = async city => {
+
+
+    baseUrl = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    queryParam = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(baseUrl + queryParam);
+    const data = await response.json();
+    return data[0];
+}
+
+// getCity('china')
+// .then(data => {
+//     return getWeather(data.Key)
+// }).then( data =>{
+//     console.log(data);
+    
+// })
+// .catch(err => console.log(err))
